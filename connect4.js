d8079f071aa89aef15435005a76c2e5ac7d8dee7
Ashley Lin <ashleyylin11@gmail.com>
2023-04-05 16:40:57 -0700

    add placeInTable and Piece CSS

--- connect4.js
+++ connect4.js
@@ -57,94 +57,98 @@ function makeHTMLBoard() {
   }
 }
 
+/** findSpotForCol: given column x, return bottom empty y (null if filled) */
 
+function findSpotForCol(x) {
+  // TODO: write the real version of this, rather than always returning 5
+  return 5;
+}
 
+/** placeInTable: update DOM to place piece into HTML table of board */
 
-  /** findSpotForCol: given column x, return bottom empty y (null if filled) */
+function placeInTable(y, x) {
+  // TODO: make a div and insert into correct table cell
+  let piece = document.createElement("div")
+  piece.classList.add("piece", `p${currPlayer}`)
+  let insertLocation = document.getElementById(`c-${y}-${x}`)
+  console.log("insert location", insertLocation)
+  console.log("coordinates", y, x)
+  insertLocation.appendChild(piece)
+}
 
-  function findSpotForCol(x) {
-    // TODO: write the real version of this, rather than always returning 5
-    return 5;
+/** endGame: announce game end */
+
+function endGame(msg) {
+  // TODO: pop up alert message
+}
+
+/** handleClick: handle click of column top to play piece */
+
+function handleClick(evt) {
+  // get x from ID of clicked cell
+  let x = Number(evt.target.id.slice('top-'.length))
+  //console.log("target.id", x)
+
+  // get next spot in column (if none, ignore click)
+  let y = findSpotForCol(x);
+  if (y === null) {
+    return;
   }
 
-  /** placeInTable: update DOM to place piece into HTML table of board */
+  // place piece in board and add to HTML table
+  // TODO: add line to update in-memory board
+  placeInTable(y, x);
 
-  function placeInTable(y, x) {
-    // TODO: make a div and insert into correct table cell
+  // check for win
+  if (checkForWin()) {
+    return endGame(`Player ${currPlayer} won!`);
   }
 
-  /** endGame: announce game end */
+  // check for tie
+  // TODO: check if all cells in board are filled; if so call, call endGame
 
-  function endGame(msg) {
-    // TODO: pop up alert message
+  // switch players
+  // TODO: switch currPlayer 1 <-> 2
+}
+
+/** checkForWin: check board cell-by-cell for "does a win start here?" */
+
+function checkForWin() {
+
+  /** _win:
+   * takes input array of 4 cell coordinates [ [y, x], [y, x], [y, x], [y, x] ]
+   * returns true if all are legal coordinates for a cell & all cells match
+   * currPlayer
+   */
+  function _win(cells) {
+
+    // TODO: Check four cells to see if they're all legal & all color of current
+    // player
+
   }
 
-  /** handleClick: handle click of column top to play piece */
+  // using HEIGHT and WIDTH, generate "check list" of coordinates
+  // for 4 cells (starting here) for each of the different
+  // ways to win: horizontal, vertical, diagonalDR, diagonalDL
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      // TODO: assign values to the below variables for each of the ways to win
+      // horizontal has been assigned for you
+      // each should be an array of 4 cell coordinates:
+      // [ [y, x], [y, x], [y, x], [y, x] ]
 
-  function handleClick(evt) {
-    // get x from ID of clicked cell
-    let x = +evt.target.id;
+      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      let vert;
+      let diagDL;
+      let diagDR;
 
-    // get next spot in column (if none, ignore click)
-    let y = findSpotForCol(x);
-    if (y === null) {
-      return;
-    }
-
-    // place piece in board and add to HTML table
-    // TODO: add line to update in-memory board
-    placeInTable(y, x);
-
-    // check for win
-    if (checkForWin()) {
-      return endGame(`Player ${currPlayer} won!`);
-    }
-
-    // check for tie
-    // TODO: check if all cells in board are filled; if so call, call endGame
-
-    // switch players
-    // TODO: switch currPlayer 1 <-> 2
-  }
-
-  /** checkForWin: check board cell-by-cell for "does a win start here?" */
-
-  function checkForWin() {
-
-    /** _win:
-     * takes input array of 4 cell coordinates [ [y, x], [y, x], [y, x], [y, x] ]
-     * returns true if all are legal coordinates for a cell & all cells match
-     * currPlayer
-     */
-    function _win(cells) {
-
-      // TODO: Check four cells to see if they're all legal & all color of current
-      // player
-
-    }
-
-    // using HEIGHT and WIDTH, generate "check list" of coordinates
-    // for 4 cells (starting here) for each of the different
-    // ways to win: horizontal, vertical, diagonalDR, diagonalDL
-    for (let y = 0; y < HEIGHT; y++) {
-      for (let x = 0; x < WIDTH; x++) {
-        // TODO: assign values to the below variables for each of the ways to win
-        // horizontal has been assigned for you
-        // each should be an array of 4 cell coordinates:
-        // [ [y, x], [y, x], [y, x], [y, x] ]
-
-        let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-        let vert;
-        let diagDL;
-        let diagDR;
-
-        // find winner (only checking each win-possibility as needed)
-        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-          return true;
-        }
+      // find winner (only checking each win-possibility as needed)
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+        return true;
       }
     }
   }
+}
 
 makeBoard();
 makeHTMLBoard();
